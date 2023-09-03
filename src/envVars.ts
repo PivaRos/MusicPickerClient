820e7bddbfa9efa5b876d675b471bcc5bec5da53
PivaRos <sbhtk478@gmail.com>
2023-09-03 15:04:29 +0300

    updated react public url

--- src/envVars.ts
+++ src/envVars.ts
@@ -1,3 +1,3 @@
-export const SERVER_PORT = ":9000/api";
-export const HOST = "http://10.0.0.13";
+export const SERVER_PORT = "";
+export const HOST = "https://danielgurbin.com/musicpicker/api";
 export const UPDATE_MS = 1000;
