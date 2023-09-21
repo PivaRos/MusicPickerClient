48aaaf68754e8b64c7f73197588088bce7e40565
PivaRos <sbhtk478@gmail.com>
2023-09-21 23:27:51 +0300

    add add/remove all searched genres

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -16,16 +16,16 @@ function Dashboard({ data, setData }: IDashboard) {
   const [validGenres, setValidGenres] = useState<Array<string>>([]);
   const [validVotes, setValidVotes] = useState<Array<string>>([]);
 
-  // New state variable for genre search
+  // New state variables for genre search
   const [genreSearchQuery, setGenreSearchQuery] = useState("");
-  const [lastFoundGenreIndex, setLastFoundGenreIndex] = useState(-1);
+  const [foundGenreIndex, setFoundGenreIndex] = useState(-1);
 
   useEffect(() => {
     setAdminPassword(adminPassword);
   }, [data.adminPassword]);
 
   useEffect(() => {
-    (async () => {
+    const fetchData = async () => {
       const genresResult = await API.Enums.getValidGenres();
       if (genresResult) {
         setValidGenres(genresResult);
@@ -34,7 +34,9 @@ function Dashboard({ data, setData }: IDashboard) {
       if (voteResult) {
         setValidVotes(voteResult);
       }
-    })();
+    };
+
+    fetchData();
   }, []);
 
   const handleEditClick = () => {
@@ -44,7 +46,7 @@ function Dashboard({ data, setData }: IDashboard) {
   const handleSaveClick = async () => {
     setIsEditing(false);
 
-    // Implement your API update logic here for each field
+    // Implement API update logic here for each field
     try {
       setLoading(true);
       await API.Admin.setAppConfig(adminPassword, editedData);
@@ -54,7 +56,7 @@ function Dashboard({ data, setData }: IDashboard) {
     }
   };
 
-  const handleInputChange = (e: any) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
 
     // Ensure that the selected genre is one of the valid genres
@@ -97,25 +99,80 @@ function Dashboard({ data, setData }: IDashboard) {
     }
 
     const lowerCaseQuery = genreSearchQuery.toLowerCase();
-    const startIndex = lastFoundGenreIndex + 1;
-    const foundGenreIndex = validGenres.findIndex(
-      (genre, index) =>
-        index >= startIndex && genre.toLowerCase().includes(lowerCaseQuery)
+
+    if (foundGenreIndex === -1) {
+      setFoundGenreIndex(0);
+    } else {
+      const nextIndex = validGenres.findIndex(
+        (genre, index) =>
+          index > foundGenreIndex &&
+          genre.toLowerCase().includes(lowerCaseQuery)
+      );
+
+      if (nextIndex !== -1) {
+        setFoundGenreIndex(nextIndex);
+      } else {
+        // No more matching genres found, start from the beginning
+        setFoundGenreIndex(0);
+      }
+    }
+
+    // Scroll to the found genre
+    const genreLabel = document.querySelector(
+      `label[data-genre="${validGenres[foundGenreIndex]}"]`
+    );
+    if (genreLabel) {
+      genreLabel.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const goBackToPreviousGenre = () => {
+    if (foundGenreIndex > 0) {
+      const previousIndex = validGenres
+        .slice(0, foundGenreIndex)
+        .reverse()
+        .findIndex((genre) =>
+          genre.toLowerCase().includes(genreSearchQuery.toLowerCase())
+        );
+
+      if (previousIndex !== -1) {
+        const newIndex = foundGenreIndex - (previousIndex + 1);
+        setFoundGenreIndex(newIndex);
+
+        // Scroll to the found genre
+        const genreLabel = document.querySelector(
+          `label[data-genre="${validGenres[newIndex]}"]`
+        );
+        if (genreLabel) {
+          genreLabel.scrollIntoView({ behavior: "smooth" });
+        }
+      }
+    }
+  };
+
+  const addAllGenres = () => {
+    // Add all genres that match the search query
+    const updatedGenres = validGenres.filter((genre) =>
+      genre.toLowerCase().includes(genreSearchQuery.toLowerCase())
     );
 
-    if (foundGenreIndex !== -1) {
-      // Scroll to the found genre
-      const genreLabel = document.querySelector(
-        `label[data-genre="${validGenres[foundGenreIndex]}"]`
-      );
-      if (genreLabel) {
-        genreLabel.scrollIntoView({ behavior: "smooth" });
-        setLastFoundGenreIndex(foundGenreIndex);
-      }
-    } else {
-      // No more matching genres found, reset the search
-      setLastFoundGenreIndex(-1);
-    }
+    setEditedData((prevData: any) => ({
+      ...prevData,
+      genres: [...prevData.genres, ...updatedGenres],
+    }));
+  };
+
+  const removeAllGenres = () => {
+    // Remove all genres that match the search query
+    const updatedGenres = editedData.genres.filter(
+      (genre: string) =>
+        !genre.toLowerCase().includes(genreSearchQuery.toLowerCase())
+    );
+
+    setEditedData((prevData: any) => ({
+      ...prevData,
+      genres: updatedGenres,
+    }));
   };
 
   return (
@@ -158,15 +215,31 @@ function Dashboard({ data, setData }: IDashboard) {
               </label>
             ))}
           </label>
-          <input
-            className="genres-search"
-            type="text"
-            placeholder="Search Genre"
-            value={genreSearchQuery}
-            onChange={(e) => setGenreSearchQuery(e.target.value)}
-          />
-          <button onClick={searchGenre}>Search</button>
-          <button onClick={() => setLastFoundGenreIndex(-1)}>
+          <div className="binder-all-search">
+            <button className="addall-button" onClick={addAllGenres}>
+              Add All {genreSearchQuery} Genres
+            </button>
+            <input
+              className="genres-search"
+              type="text"
+              placeholder="Search Genre"
+              value={genreSearchQuery}
+              onChange={(e) => setGenreSearchQuery(e.target.value)}
+            />
+            <button className="removeall-button" onClick={removeAllGenres}>
+              Remove All {genreSearchQuery} Genres
+            </button>
+          </div>
+          <button className="search-button" onClick={searchGenre}>
+            Search
+          </button>
+          <button className="back-button" onClick={goBackToPreviousGenre}>
+            Back
+          </button>
+          <button
+            className="reset-button"
+            onClick={() => setFoundGenreIndex(-1)}
+          >
             Reset Search
           </button>
           <label
